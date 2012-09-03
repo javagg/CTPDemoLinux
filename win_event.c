04581d3c3b29dc341a6e4f6ece030507205ebad6
alex <alex@AlexLee>
2012-09-03 21:25:28 +0800

    win_event implementation

--- win_event.c
+++ win_event.c
@@ -1,16 +1,65 @@
 #include "win_event.h"
 #include <pthread.h>
 
-HANDLE CreateEvent(void *lpEventAttributes, BOOL bManualReset, BOOL bInitialState, void *lpName) {
-    return 0;
+
+
+struct event_flag* eventflag_create()
+{
+    struct event_flag* ev;
+    ev = (struct event_flag*) malloc(sizeof(struct event_flag));
+
+    pthread_mutex_init(&ev->mutex, NULL);
+    pthread_cond_init(&ev->condition, NULL);
+    ev->flag = 0;
+
+    return ev;
 }
 
-DWORD WaitForSingleObject(HANDLE hHandle, DWORD dwMilliseconds) {
+void eventflag_wait(struct event_flag* ev)
+{
+    pthread_mutex_lock(&ev->mutex);
+
+    while (!ev->flag)
+        pthread_cond_wait(&ev->condition, &ev->mutex);
+
+    ev->flag = 0;
+
+    pthread_mutex_unlock(&ev->mutex);
+}
+
+void eventflag_set(struct event_flag* ev)
+{
+    pthread_mutex_lock(&ev->mutex);
+
+    ev->flag = 1;
+
+    pthread_mutex_unlock(&ev->mutex);
+
+    pthread_cond_signal(&ev->condition);
+
+
+//    pthread_mutex_lock(&ev->mutex);
+
+//    ev->flag = 1;
+//    pthread_cond_signal(&ev->condition);
+
+//    pthread_mutex_unlock(&ev->mutex);
+}
+
+HANDLE CreateEvent(void *lpEventAttributes, BOOL bManualReset, BOOL bInitialState, void *lpName) {
+
+    return eventflag_create();
+
+}
+
+DWORD WaitForSingleObject(HANDLE hEvent, DWORD dwMilliseconds) {
+    eventflag_wait(hEvent);
     return 0;
 }
 BOOL ResetEvent(HANDLE hEvent) {
     return 0;
 }
 BOOL SetEvent(HANDLE hEvent) {
-    return 0;
+     eventflag_set(hEvent);
+     return 1;
 }
--- win_event.h
+++ win_event.h
@@ -1,18 +1,27 @@
 #include <stdint.h>
+#include <pthread.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+struct event_flag
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t  condition;
+    unsigned int    flag;
+};
+
 typedef int32_t DWORD;
-typedef int HANDLE;
+typedef struct event_flag *HANDLE;
 typedef int BOOL;
 typedef const char * LPCTSTR;
+
 
 #define INFINITE 0
 
 HANDLE CreateEvent(void *lpEventAttributes, BOOL bManualReset, BOOL bInitialState, void *lpName);
-DWORD WaitForSingleObject(HANDLE hHandle, DWORD dwMilliseconds);
+DWORD WaitForSingleObject(HANDLE hEvent, DWORD dwMilliseconds);
 BOOL ResetEvent(HANDLE hEvent);
 BOOL SetEvent(HANDLE hEvent);
 
